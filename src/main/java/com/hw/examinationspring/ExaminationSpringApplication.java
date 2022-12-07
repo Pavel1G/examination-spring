cff84f1e1c051ac11911bb0728b42f8ae43ea98b
Pavel1G <94784915+Pavel1G@users.noreply.github.com>
2022-12-07 22:54:24 +0300

    Initial commit

---
+++ src/main/java/com/hw/examinationspring/ExaminationSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hw.examinationspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminationSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hw/examinationspring/ExaminationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.examinationspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
